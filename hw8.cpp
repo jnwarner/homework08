c77b9cf1a4e8e3a2631a90fb8df3bae3979234ce
Bray <atb9k6@rc07xcs213.managed.mst.edu>
2016-11-05 20:07:54 -0500

    Header and main comments

---
+++ hw8.cpp
@@ -0,0 +1,6 @@
+//Programmers: ANDREW BRAY, JOSHUA WARNER    IDs: 12518487, 12522483
+//Class: cs1570                              Sections: A, B
+//Date: 11/05/16                             File: hw8.h
+//Description:
+
+#include "hw8.h"
---
+++ hw8.h
@@ -0,0 +1,19 @@
+//Programmers: ANDREW BRAY, JOSHUA WARNER         IDs: 12518487, 12522483
+//Class: cs1570                              Sections: A, B
+//Date: 11/05/16                             File: hw8.h
+//Description:
+
+
+#ifndef HW8.H
+#define HW8.H
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cctype>
+#include <fstream>
+using namespace std;
+
+
+
+
+#endif
